b20681e8e1ec0fc98d43ed463cc9889e2f519f99
osmanforhad <osmanforhad93@gmail.com>
2025-07-22 11:42:24 +0600

    working with json data through the request body using body-parser package

--- jesonResponse.js
+++ jesonResponse.js
@@ -1,11 +1,15 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 
-app = express();
+var app = express();
+//body-parser for work with JSON Data
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     res.send("This is simple string response");
 });
 
+//json respons with get method
 app.get('/two', (req, res) => {
     let myJesonArray = [
         {
@@ -18,6 +22,23 @@ app.get('/two', (req, res) => {
         }
     ];
     res.status(200).json(myJesonArray);
+});
+
+//json response with post method
+app.post('/three', (req, res) => {
+    //fetch request body
+    let jsonData = req.body;
+    //to display json as output
+    let jsonResult = JSON.stringify(jsonData);
+    //response body
+    res.send(jsonResult);
+
+    //display specific json as output
+    let name = jsonData['name'];
+    let city = jsonData['city'];
+    //response body
+    res.send(name + " " + city);
+
 });
 
 
