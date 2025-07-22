728548ca750d5f24d1a567c1e3668ce2b5d23061
osmanforhad <osmanforhad93@gmail.com>
2025-07-22 11:27:18 +0600

    working with request header using post method

--- requestHeader.js
+++ requestHeader.js
@@ -2,11 +2,21 @@ var express = require('express');
 
 app = express();
 
+//request header with get method
 app.get('/', (req, res) => {
     let fName = req.header('firstName');
     let lName = req.header('lastName');
 
     res.send(fName + " " + lName);
+});
+
+//request header with post method
+app.post('/two', (req, res) => {
+    //request header
+    let uName = req.header('userName');
+    let uPass = req.header('password');
+    //response body
+    res.send("User Name: " + uName + " " + "Password: " + uPass);
 });
 
 
